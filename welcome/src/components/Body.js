db13bc4ead384743023694dd9d5fe054ebd4d32e
CHATHURANGA FORM UOVT <chathurangaFromUovt@gmail.com>
2025-03-03 08:31:17 +0530

    02

---
+++ welcome/src/components/Body.js
@@ -0,0 +1,19 @@
+import '../css/Body.css'
+
+function Body() {
+
+    return (
+
+        <div>
+
+            <div className="body">
+
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Body;
---
+++ welcome/src/components/Footer.js
@@ -0,0 +1,47 @@
+function Footer() {
+
+    return (
+
+        <div>
+
+            <footer class="bg-dark text-white text-center text-lg-start mt-5">
+                <div class="container p-4">
+                    <div class="row">
+
+                        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                            <h5 class="text-uppercase">MyWebsite</h5>
+                            <p>We provide the best solutions for your needs. Contact us anytime.</p>
+                        </div>
+
+
+                        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                            <h5 class="text-uppercase">Quick Links</h5>
+                            <ul class="list-unstyled mb-0">
+                                <li><a href="#" class="text-white">Home</a></li>
+                                <li><a href="#" class="text-white">About</a></li>
+                                <li><a href="#" class="text-white">Services</a></li>
+                                <li><a href="#" class="text-white">Contact</a></li>
+                            </ul>
+                        </div>
+
+
+                        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                            <h5 class="text-uppercase">Follow Us</h5>
+                            <a href="#" class="text-white me-4"><i class="bi bi-facebook"></i> Facebook</a><br />
+                            <a href="#" class="text-white me-4"><i class="bi bi-twitter"></i> Twitter</a><br />
+                            <a href="#" class="text-white me-4"><i class="bi bi-instagram"></i> Instagram</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="text-center p-3 bg-secondary">
+                    © 2025 MyWebsite. All Rights Reserved.
+                </div>
+
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
